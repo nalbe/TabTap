14afd8ddf94bfd19ab689baa7194e8d099896e95
nalbe666 <nalbe@ya.ru>
2025-03-06 23:52:24 +0300

    Add files via upload

---
+++ src/Registry.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+bool ReadRegistry(bool, const TCHAR*, const TCHAR*, void*);
+
+bool WriteRegistry(bool, const TCHAR*, const TCHAR*, void*, DWORD);
+
+bool RemoveRegistry(bool, const TCHAR*, const TCHAR*);
+
+
+
