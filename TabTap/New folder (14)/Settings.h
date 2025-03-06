c3330e769f4f1d6a06ceb6a726b75a5453d3b284
nalbe666 <nalbe@ya.ru>
2025-03-06 22:28:42 +0300

    Delete TabTap directory
    ops

--- TabTap/New folder (14)/Settings.h
+++
@@ -1,8 +0,0 @@
-#ifndef SETTINGS_WINDOW_H
-#define SETTINGS_WINDOW_H
-
-// Displays a settings window with checkboxes and buttons.
-// This function runs its own message loop until the window is closed.
-void ShowSettingsWindow(HINSTANCE hInstance);
-
-#endif // SETTINGS_WINDOW_H
--- TabTap/New folder (15)/Menu.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-int WINAPI menuWinMain(
-	_In_ HINSTANCE,
-	_In_opt_ HINSTANCE,
-	_In_ LPSTR,
-	_In_ int
-);
